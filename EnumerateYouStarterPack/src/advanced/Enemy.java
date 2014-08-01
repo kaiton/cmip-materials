d9996c9f6a19a215dccdfa371d70ed51717d7f68
Kaiton Williams <kaiton@cs.cornell.edu>
2014-07-31 18:01:18 -0700

    fleshing out enemies and players

--- EnumerateYouStarterPack/src/advanced/Enemy.java
+++ EnumerateYouStarterPack/src/advanced/Enemy.java
@@ -1,15 +1,25 @@
 package advanced;
 enum Enemy {
-	Dragon(10), Turtle(14), Ninja(34), Kitten(-10), Wolf(13);
+	Dragon(100, 10), Turtle(60, 14), Ninja(30,34), Kitten(1,-10), Wolf(30, 13);
 	
 	private int points;
+	private int lifeMeter;
 	
-	Enemy(int points){
+	Enemy(int lifeMeter, int points){
 		this.points = points;
+		this.lifeMeter = lifeMeter;
 	}
 	
 	int getPoints(){
 		return this.points;
+	}
+	
+	int getLifeMeter() {
+		return this.lifeMeter;
+	}
+	
+	void hit(Player player){
+		
 	}
 }
 	
--- EnumerateYouStarterPack/src/advanced/Player.java
+++ EnumerateYouStarterPack/src/advanced/Player.java
@@ -2,4 +2,23 @@ package advanced;
 
 class Player {
 	
+	Weapon weapon; 
+	Skill skill;
+	Armour armour;
+	
+	int lifeMeter;
+	int points;
+	
+	void hit(Enemy enemy){
+	}
+	
+	void eatFood(Food food){		
+	}
+	
+	void putOnArmour(Armour armour){
+	}
+	
+	void getWeapon(Weapon weapon){
+	}
+
 }
